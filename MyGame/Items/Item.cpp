ec27e40f3ca0c0faebedaf507f057945a70ccff9
Claudia <talo930810@gmail.com>
2025-06-07 15:51:32 +0800

    commit 25

--- MyGame/Items/Item.cpp
+++ MyGame/Items/Item.cpp
@@ -4,7 +4,15 @@
 #include "Item.hpp"
 #include "Engine/Point.hpp"
 #include "Engine/Sprite.hpp"
+#include "Engine/GameEngine.hpp"
+#include "Scene/MapScene.hpp"
 #include "UI/Component/Image.hpp"
+
+class MapScene;
+
+MapScene *Item::getMapScene(){
+    return dynamic_cast<MapScene*>(Engine::GameEngine::GetInstance().GetActiveScene());
+}
 
 Item::Item(std::string img, float x, float y, std::string t):Sprite(img, x, y, 30, 30), image_name(img), type(t), picked(false){
     CollisionRadius = 5;
@@ -14,6 +22,7 @@ void Item::Pick(){
     if(picked) return;
     picked = true;
     Visible = false;
+    getMapScene()->PickupItem(this);
 }
 
 void Item::Update(float deltaTime) {
@@ -22,7 +31,6 @@ void Item::Update(float deltaTime) {
     Sprite::Update(deltaTime); // update animation if needed
     return;
 }
-
 
 bool Item::item_picked(){
     return picked;
